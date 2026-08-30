bdb95a90cf58e914963af3a148931213570dda4f
DarioL <dario.herrera@jala.university>
2026-08-30 01:56:16 -0400

    fix: remove strip_path from oathkeeper so kratos can match its base_url routes correctly

--- frontend/src/lib/signalr.ts
+++
@@ -1,9 +0,0 @@
-import * as signalR from "@microsoft/signalr";
-import { API_URL } from "./config";
-
-export function createHubConnection(hubPath: string): signalR.HubConnection {
-  return new signalR.HubConnectionBuilder()
-    .withUrl(`${API_URL}${hubPath}`, { withCredentials: true })
-    .withAutomaticReconnect()
-    .build();
-}
